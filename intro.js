4fdd453958aa758f1d4bb1dffc0b816dcdb93823
Ibrahim <ibrahimmuhammad018@gmail.com>
2022-06-24 05:43:58 +0000

    first commit

---
+++ intro.js
@@ -0,0 +1,10 @@
+console.log("JavaScript - Task 1")
+
+let Name = "Ibrahim Mohammed"
+let height = 6.2
+let country = "Ghana"
+
+
+console.log(`${Name}` )
+console.log(`${height}ft`)
+console.log(`${country}`) 
